3f427fe3d01e2a9d792bb1a67686a7f4f96fe32b
gyd <yadiq@qq.com>
2024-12-04 16:55:52 +0800

    1.升级gradle配置

--- app/src/main/java/com/hqumath/demo/Density.java
+++ app/src/main/java/com/hqumath/demo/Density.java
@@ -5,8 +5,9 @@ import android.app.Application;
 import android.content.ComponentCallbacks;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
+
+import androidx.annotation.NonNull;
 
 /**
  * ****************************************************************
